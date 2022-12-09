b5d97eb8cb649b42d7cd7cf65f14a9b277e1e3a2
Andrew Gillard <andrew.gillard@gmail.com>
2022-12-09 11:45:40 +0000

    Simple 9-element dict lookup solution

--- day02/day02.py
+++ day02/day02.py
@@ -1,6 +1,8 @@
 # A = Rock, B = Paper, C = Scissors
 # Response: X = Rock, Y = Paper, Z = Scissors
-# Winner: A beats Y, C beat Y, B beats X
+# Winner: A beats Z, C beats Y, B beats X
+
+result_cheatsheet = {'A X': 4, 'A Y': 8, 'A Z': 3, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 7, 'C Y': 2, 'C Z': 6}
 
 type_scores = {'A': 1, 'X': 1, 'B': 2, 'Y': 2, 'C': 3, 'Z': 3}
 win_score = {'win':6, 'draw':3, 'loss':0}
@@ -8,34 +10,37 @@ win_score = {'win':6, 'draw':3, 'loss':0}
 def part1():
     total_score = 0
     for line in file.readlines():
-        Opponent = line.split()[0]
-        Player = line.split()[1]
-        round_score = type_scores[Player]
-        if (Opponent == 'A'):
-            if (Player == 'X'):
-                round_score += win_score['draw']
-            elif (Player == 'Y'):
-                round_score += win_score['win']
-            elif (Player == 'Z'):
-                round_score += win_score['loss']
-        elif (Opponent == 'B'):
-            if (Player == 'X'):
-                round_score += win_score['loss']
-            elif (Player == 'Y'):
-                round_score += win_score['draw']
-            elif (Player == 'Z'):
-                round_score += win_score['win']
-        elif (Opponent == 'C'):
-            if (Player == 'X'):
-                round_score += win_score['win']
-            elif (Player == 'Y'):
-                round_score += win_score['loss']
-            elif (Player == 'Z'):
-                round_score += win_score['draw']
-        total_score += round_score
+        total_score += result_cheatsheet.get(line.rstrip())
+        # Opponent = line.split()[0]
+        # Player = line.split()[1]
+        # round_score = type_scores[Player]
+        # if (Opponent == 'A'):
+        #     if (Player == 'X'):
+        #         round_score += win_score['draw']
+        #     elif (Player == 'Y'):
+        #         round_score += win_score['win']
+        #     elif (Player == 'Z'):
+        #         round_score += win_score['loss']
+        # elif (Opponent == 'B'):
+        #     if (Player == 'X'):
+        #         round_score += win_score['loss']
+        #     elif (Player == 'Y'):
+        #         round_score += win_score['draw']
+        #     elif (Player == 'Z'):
+        #         round_score += win_score['win']
+        # elif (Opponent == 'C'):
+        #     if (Player == 'X'):
+        #         round_score += win_score['win']
+        #     elif (Player == 'Y'):
+        #         round_score += win_score['loss']
+        #     elif (Player == 'Z'):
+        #         round_score += win_score['draw']
+        # total_score += round_score
     return total_score
 
 def part2():
+    file.seek(0)
+
     total_score = 0
     for line in file.readlines():
         Opponent = line.split()[0]
@@ -43,35 +48,44 @@ def part2():
         round_score = 0
         if (Opponent == 'A'):
             if (Outcome == 'X'):
-                round_score += type_scores['C']
-                round_score += win_score['loss']
+                total_score += result_cheatsheet.get('A Z')
+                # round_score += type_scores['C']
+                # round_score += win_score['loss']
             elif (Outcome == 'Y'):
-                round_score += type_scores[Opponent]
-                round_score += win_score['draw']
+                total_score += result_cheatsheet.get('A X')
+                # round_score += type_scores[Opponent]
+                # round_score += win_score['draw']
             elif (Outcome == 'Z'):
-                round_score += type_scores['B']
-                round_score += win_score['win']
+                total_score += result_cheatsheet.get('A Y')
+                # round_score += type_scores['B']
+                # round_score += win_score['win']
         elif (Opponent == 'B'):
             if (Outcome == 'X'):
-                round_score += type_scores['A']
-                round_score += win_score['loss']
+                total_score += result_cheatsheet.get('B X')
+                # round_score += type_scores['A']
+                # round_score += win_score['loss']
             elif (Outcome == 'Y'):
-                round_score += type_scores[Opponent]
-                round_score += win_score['draw']
+                total_score += result_cheatsheet.get('B Y')
+                # round_score += type_scores[Opponent]
+                # round_score += win_score['draw']
             elif (Outcome == 'Z'):
-                round_score += type_scores['C']
-                round_score += win_score['win']
+                total_score += result_cheatsheet.get('B Z')
+                # round_score += type_scores['C']
+                # round_score += win_score['win']
         elif (Opponent == 'C'):
             if (Outcome == 'X'):
-                round_score += type_scores['B']
-                round_score += win_score['loss']
+                total_score += result_cheatsheet.get('C Y')
+                # round_score += type_scores['B']
+                # round_score += win_score['loss']
             elif (Outcome == 'Y'):
-                round_score += type_scores[Opponent]
-                round_score += win_score['draw']
+                total_score += result_cheatsheet.get('C Z')
+                # round_score += type_scores[Opponent]
+                # round_score += win_score['draw']
             elif (Outcome == 'Z'):
-                round_score += type_scores['A']
-                round_score += win_score['win']
-        total_score += round_score
+                total_score += result_cheatsheet.get('C X')
+                # round_score += type_scores['A']
+                # round_score += win_score['win']
+        # total_score += round_score
     return total_score
 
 
@@ -81,5 +95,5 @@ if Testing:
 else:
     file = open("input.txt", "r")
 
-# print("Part 1: ", part1())
+print("Part 1: ", part1())
 print("Part 2: ", part2())
