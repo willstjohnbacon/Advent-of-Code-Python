ea3a42185dc5471b7dd80ff2dd1def5b0170de45
Snakehead181 <ralf.dog@outlook.com>
2022-12-10 05:58:22 +0000

    Day 10, Part 2

--- TEMPLATE/day10.py
+++ TEMPLATE/day10.py
@@ -14,8 +14,25 @@ def part1():
     return signal_strength
 
 def part2():
-    file.seek(0)
-    return
+    program = [line.split() for line in file]
+    cycle = 0
+    register = 1
+    signal_strength = 0
+    display = []
+    row = ""
+    for instruction in program:
+        for c in range(1 if instruction[0] == "noop" else 2):
+            cycle += 1
+            row += "#" if len(row) in range(register - 1, register + 2) else "."
+            if len(row) == 40:
+                display.append(row)
+                row = ""
+            if cycle in (20, 60, 100, 140, 180, 220):
+                signal_strength += cycle * register
+        if instruction[0] == "addx":
+            register += int(instruction[1])
+    # Use the * to print the lines on different lines
+    return print(*display, sep="\n")
 
 
 Testing = False
@@ -26,4 +43,6 @@ else:
     file = open("input.txt", "r")
 
 print("Part 1: ", part1())
+file.close()
+file = open("input.txt", "r")
 print("Part 2: ", part2())
