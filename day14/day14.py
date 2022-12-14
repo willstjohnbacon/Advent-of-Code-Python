4e798537d31a90649722d78aa55dbf651b0a3c91
Snakehead181 <ralf.dog@outlook.com>
2022-12-14 12:05:49 +0000

    Day14, WIP

--- day14/day14.py
+++ day14/day14.py
@@ -1,9 +1,72 @@
 TESTING = True
 
+xpoints = []
+
+def printGrid(width, height):
+    y = 1
+    print('  ', end='')
+    for x in range(0, width):
+        print(y, end='')
+        y = y + 1
+        if y == 10:
+            y = 0
+    print('')
+
+    print(' +', '-' * (width), sep='')
+    z = 1
+    for i in range(0, height):
+        line = str(z) + "|"
+        for x in range(width):
+            if [x, i] in xpoints:
+                line += 'X'
+            else:
+                line += '.'
+        print(line)
+        z = z + 1
+        if z == 10:
+            z = 0
+
+def axisSize(lines):
+    highest_point_x = 0
+    lowest_point_x = 1000
+    highest_point_y = 0
+    lowest_point_y = 1000
+    for points in lines:
+        current_line = points.split(' -> ')
+        for point in current_line:
+            print(point)
+            x = int(point[0:3])
+            y = int(point[4:])
+            if highest_point_x < x:
+                highest_point_x = x
+            elif lowest_point_x > x:
+                lowest_point_x = x
+
+            if highest_point_y < y:
+                highest_point_y = y
+            elif lowest_point_y > y:
+                lowest_point_y = y
+
+    print(lowest_point_x, highest_point_x)
+    print(lowest_point_y, highest_point_y)
+
 def part1():
     file.seek(0)
     lines = [line.rstrip() for line in file]
-    print ("Input lines:", lines)
+    # width = 551 - 492
+    # height = 177 - 14
+    width = 503 - 498
+    height = 9 - 4
+    for points in lines:
+        current_line = points.split(' -> ')
+        for point in current_line:
+            x = int(point[0:3])
+            y = int(point[4:])
+            # xpoints.append([551-x, 177-y])
+            print(x, y)
+            xpoints.append([503-x, 9-y])
+
+    printGrid(width, height)
     return
 
 def part2():
