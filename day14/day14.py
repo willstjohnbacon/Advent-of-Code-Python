84d9ea9f19ab51b86db0c28f82f99fe7dc84ab27
Andrew Gillard <andrew.gillard@gmail.com>
2022-12-14 22:08:00 +0000

    Day 14 tidy up

--- day14/day14.py
+++ day14/day14.py
@@ -46,15 +46,7 @@ def flowSand(cave, min_width, until_full):
             sand_posX = new_posX
             sand_posY = new_posY
 
-def part1():
-    if TESTING:
-        file = open("sampleInput.txt", "r")
-    else:
-        file = open("input.txt", "r")
-
-    file.seek(0)
-    formations = [line.rstrip().split(" -> ") for line in file]
-
+def determineDimensions(formations):
     min_width = 999999999
     max_width = 0
     depth = 0
@@ -74,10 +66,10 @@ def part1():
 
     max_width += 1
     depth += 1
-    cave = [["." for x in range(max_width - min_width)] for y in range(depth)]
 
-    cave[0][500 - min_width] = "+"
+    return min_width, max_width, depth
 
+def mapCave (cave, formations, min_width):
     for formation in formations:
         for pointnum in range(len(formation) - 1):
             x1 = int(formation[pointnum][0:3]) - min_width
@@ -96,6 +88,21 @@ def part1():
                 for y in range(min(y1, y2), max(y1, y2) + 1):
                     cave[y][x1] = "#"
 
+def part1():
+    if TESTING:
+        file = open("sampleInput.txt", "r")
+    else:
+        file = open("input.txt", "r")
+
+    file.seek(0)
+    formations = [line.rstrip().split(" -> ") for line in file]
+
+    min_width, max_width, depth = determineDimensions(formations)
+
+    cave = [["." for x in range(max_width - min_width)] for y in range(depth)]
+    cave[0][500 - min_width] = "+"
+
+    mapCave(cave, formations, min_width)
     printCave(cave)
 
     sand_at_rest = flowSand(cave, min_width, False)
@@ -111,47 +118,12 @@ def part2():
     file.seek(0)
     formations = [line.rstrip().split(" -> ") for line in file]
 
-    min_width = 999999999
-    max_width = 0
-    depth = 0
+    min_width, max_width, depth = determineDimensions(formations)
 
-    for formation in formations:
-        for pointnum in range(len(formation)):
-            x = int(formation[pointnum][0:3])
-            y = int(formation[pointnum][4:])
-            if x < min_width:
-                min_width = x
-            if x > max_width:
-                max_width = x
-            if y > depth:
-                depth = y
-
-    print(f"Cave span {min_width} to {max_width} and depth {depth}")
-
-    max_width += 1
-    depth += 1
     cave = [["." for x in range(max_width - min_width)] for y in range(depth)]
-
     cave[0][500 - min_width] = "+"
 
-    for formation in formations:
-        for pointnum in range(len(formation) - 1):
-            x1 = int(formation[pointnum][0:3]) - min_width
-            y1 = int(formation[pointnum][4:])
-            x2 = int(formation[pointnum + 1][0:3]) - min_width
-            y2 = int(formation[pointnum + 1][4:])
-
-            if (x1 != x2) and (y1 != y2):
-                print(f"ERROR: diagonal line in {formation}: ({x1},{y1}) -> ({x2},{y2}")
-                exit(1)
-
-            if (y1 == y2): #horizontal line
-                for x in range(min(x1, x2), max(x1, x2) + 1):
-                    cave[y1][x] = "#"
-            else: #vertical line
-                for y in range(min(y1, y2), max(y1, y2) + 1):
-                    cave[y][x1] = "#"
-
+    mapCave(cave, formations, min_width)
     printCave(cave)
 
     sand_at_rest = flowSand(cave, min_width, True)
