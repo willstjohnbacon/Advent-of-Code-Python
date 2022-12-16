b0876a217e32e9d5c91a01410511b99e477882d5
Andrew Gillard <andrew.gillard@gmail.com>
2022-12-16 10:36:29 +0000

    Day 16 determining shortest paths between valves

--- day16/day16.py
+++ day16/day16.py
@@ -1,19 +1,65 @@
 import re
+from dijkstra import *
 
-TESTING = True
+TESTING = False
 
-def readValves(lines):
+def readValves(lines, valves):
     for line in lines:
         raw_data = re.match('Valve (.*?) has flow rate=(.*?); tunnel[s]? lead[s]? to valve[s]? (.*?)$', line)
-        valve = raw_data[1]
+        name = raw_data[1]
         flow_rate = raw_data[2]
-        connecting_valves = raw_data[3].split()
-        print(f"Valve {valve} flows at {flow_rate} and connects to {connecting_valves}")
+        connecting_valves = raw_data[3].split(", ")
+        valves.append({"name" : name, "flow_rate" : flow_rate, "connections" : connecting_valves})
+        print(f"Valve {name} flows at {flow_rate} and connects to {connecting_valves}")
+
+def printGraph(graph):
+    print('Graph data:')
+    for v in graph:
+        for w in v.get_connections():
+            vid = v.get_id()
+            wid = w.get_id()
+            print('( %s, %s, %3d)' % (vid, wid, v.get_weight(w)))
+
+def printPaths(paths):
+    for route, path in paths.items():
+        print(f"{route[0]} -> {route[1]} = {path}")
 
 def part1():
     file.seek(0)
     lines = [line.rstrip() for line in file]
-    readValves(lines)
+
+    valves = []
+    paths = {}
+
+    readValves(lines, valves)
+
+    graph = Graph()
+
+    for valve in valves:
+        graph.add_vertex(valve.get("name"))
+        for connecting_valve in valve.get("connections"):
+            graph.add_edge(valve.get("name"), connecting_valve, 1)
+
+    printGraph(graph)
+
+    for source in valves:
+        source_name = source.get("name")
+
+        for target in valves:
+            target_name = target.get("name")
+
+            if (source_name != target_name):
+                dijkstra(graph, graph.get_vertex(source_name), graph.get_vertex(target_name))
+
+                target_vertex = graph.get_vertex(target_name)
+                path = [target_vertex.get_id()]
+                shortest(target_vertex, path)
+
+                print('The shortest path : %s' % (path[::-1]))
+
+                paths.update({(source_name, target_name) : path[::-1]})
+
+    printPaths(paths)
     return
 
 def part2():
