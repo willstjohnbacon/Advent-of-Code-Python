757b6e7ca69baddc9d6e1947701407d82b2ccce2
Andrew Gillard <andrew.gillard@gmail.com>
2022-12-16 13:36:44 +0000

    Day 16 algorithm in place to determine max pressure release but not working with sample data and too inefficient

--- day16/day16.py
+++ day16/day16.py
@@ -9,7 +9,7 @@ def readValves(lines, valves):
         name = raw_data[1]
         flow_rate = raw_data[2]
         connecting_valves = raw_data[3].split(", ")
-        valves.append({"name" : name, "flow_rate" : flow_rate, "connections" : connecting_valves})
+        valves.update({name: {"flow_rate" : flow_rate, "connections" : connecting_valves}})
         print(f"Valve {name} flows at {flow_rate} and connects to {connecting_valves}")
 
 def printGraph(graph):
@@ -24,11 +24,45 @@ def printPaths(paths, distances):
     for route, path in paths.items():
         print(f"{route[0]} -> {route[1]} = {path} [Dist {distances.get(route)}]")
 
+def calcMaxValue(valve_name, valves, distances, open_valves, time_remaining):
+    if (time_remaining == 0):
+        return 0
+
+    attributes = valves.get(valve_name)
+
+    #Option 1 - Move immediately
+    max_next_value = 0
+
+    for next_valve_name in attributes.get("connections"):
+        new_time_remaining = time_remaining - distances.get((valve_name, next_valve_name))
+
+        if new_time_remaining > 0:
+            max_next_value = max(max_next_value, calcMaxValue(next_valve_name, valves, distances, open_valves, new_time_remaining))
+
+    # Option 2 - Open Valve then Move - only if not already open and flow_rate > 0
+    flow_rate = attributes.get("flow_rate")
+
+    if (valve_name in open_valves) or flow_rate == 0:
+        return max_next_value
+
+    new_open_valves = open_valves.copy()
+    new_open_valves.append(valve_name)
+
+    open_valve_value = int(flow_rate) * (time_remaining - 1)
+
+    for next_valve_name in attributes.get("connections"):
+        new_time_remaining = time_remaining - 1 - distances.get((valve_name, next_valve_name))
+
+        if new_time_remaining > 0:
+            max_next_value = max(max_next_value, open_valve_value + calcMaxValue(next_valve_name, valves, distances, new_open_valves, new_time_remaining))
+
+    return max_next_value
+
 def part1():
     file.seek(0)
     lines = [line.rstrip() for line in file]
 
-    valves = []
+    valves = {}
     paths = {}
     distances = {}
 
@@ -36,19 +70,15 @@ def part1():
 
     graph = Graph()
 
-    for valve in valves:
-        graph.add_vertex(valve.get("name"))
-        for connecting_valve in valve.get("connections"):
-            graph.add_edge(valve.get("name"), connecting_valve, 1)
+    for valve_name, valve_attributes in valves.items():
+        graph.add_vertex(valve_name)
+        for connecting_valve in valve_attributes.get("connections"):
+            graph.add_edge(valve_name, connecting_valve, 1)
 
     printGraph(graph)
 
-    for source in valves:
-        source_name = source.get("name")
-
-        for target in valves:
-            target_name = target.get("name")
-
+    for source_name in valves.keys():
+        for target_name in valves.keys():
             if (source_name != target_name):
                 dijkstra(graph, graph.get_vertex(source_name), graph.get_vertex(target_name))
 
@@ -62,7 +92,8 @@ def part1():
                 distances.update({(source_name, target_name) : len(path)})
 
     printPaths(paths, distances)
-    return
+
+    return calcMaxValue("AA", valves, distances, [], 30)
 
 def part2():
     file.seek(0)
