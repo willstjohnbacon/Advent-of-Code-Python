3279e359b789d730277bef7f3d0e6f9180b3cf9f
Andrew Gillard <andrew.gillard@gmail.com>
2022-12-28 12:00:35 +0000

    Day 25 Part 1 complete

--- day25/day25.py
+++ day25/day25.py
@@ -1,10 +1,46 @@
-TESTING = True
+TESTING = False
+
+def add(num1, num2):
+    snafu_to_val = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2}
+    val_to_snafu = {-2: "=", -1: "-", 0: "0", 1: "1", 2: "2"}
+
+    snafu_sum = ""
+    carry = 0
+
+    for digit_num in range(len(num1) - 1, -1, -1):
+        digit1 = snafu_to_val[num1[digit_num]]
+        digit2 = snafu_to_val[num2[digit_num]]
+
+        sum_digit = (digit1 + digit2) + carry
+        carry = 0
+
+        if sum_digit > 2:
+            carry = 1
+            sum_digit -= 5
+        elif sum_digit < -2:
+            carry = -1
+            sum_digit += 5
+
+        snafu_sum = val_to_snafu[sum_digit] + snafu_sum
+
+    if carry:
+        snafu_sum = val_to_snafu[carry] + snafu_sum
+
+    return snafu_sum
 
 def part1():
     file.seek(0)
-    lines = [line.rstrip() for line in file]
-    print ("Input lines:", lines)
-    return
+    nums = [line.rstrip().zfill(20) for line in file]
+
+    snafu_sum = "00000000000000000000"
+
+    for num in nums:
+        snafu_sum = add(snafu_sum, num)
+
+    # print(add("1=", "1="))
+    # print(add("2=", "2="))
+    # print(add("2=0=", "2=01"))
+    return snafu_sum
 
 def part2():
     file.seek(0)
