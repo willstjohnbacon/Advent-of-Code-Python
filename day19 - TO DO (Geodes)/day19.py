d8ae987b737819ba284ed197ee5f124c8b25f4e2
Snakehead181 <ralf.dog@outlook.com>
2022-12-30 09:18:28 +0000

    Day 19 progress

--- day19 - TO DO (Geodes)/day19.py
+++ day19 - TO DO (Geodes)/day19.py
@@ -1,9 +1,54 @@
+import re
+import sys
+
 TESTING = True
+
+ore_collecting_robots = 0
+clay_collecting_robots = 0
+
+
+class blueprint:
+    __slots__ = ("id", "cost", "useful")
+    def __init__(self, input_string: str) -> None:
+        values = [int(i) for i in re.findall(r"\d+", input_string)]
+        self.id = values[0]
+        self.cost = {
+            "ore": {"ore": values[1]},
+            "clay": {"ore": values[2]},
+            "obsidian": {"ore": values[3], "clay": values[4]},
+            "geode": {"ore": values[5], "obsidian": values[6]}
+        }
+        self.useful = {
+            "ore": max(self.cost["clay"]["ore"],
+                       self.cost["obsidian"]["ore"],
+                       self.cost["geode"]["ore"]),
+            "clay": self.cost["obsidian"]["clay"],
+            "obsidian": self.cost["geode"]["obsidian"],
+            "geode": float("inf")
+        }
+
+class State:
+    __slots__ = ("robots", "resources", "ignored")
+
+    def __init__(self, robots: dict = None, resources: dict = None,
+                 ignored: list = None):
+        self.robots = robots.copy() if robots else {
+            "ore": 1, "clay": 0, "obsidian": 0, "geode": 0
+        }
+        self.resources = resources.copy() if resources else {
+            "ore": 0, "clay": 0, "obsidian": 0, "geode": 0
+        }
+        self.ignored = ignored.copy() if ignored else []
+
+    def copy(self) -> "State":
+        return
+    def __gt__(self, other):
+        return
+    def __repr__(self):
+        return
 
 def part1():
     file.seek(0)
-    lines = [line.rstrip() for line in file]
-    print ("Input lines:", lines)
     return
 
 def part2():
